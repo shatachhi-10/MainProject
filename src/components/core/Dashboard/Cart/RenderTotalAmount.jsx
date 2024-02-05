af5bff5ea600abb9c1b0a1f53bbff2ac849ae11a
shatachhi-10 <137662646+shatachhi-10@users.noreply.github.com>
2024-02-05 11:44:30 +0530

    Payment Gateway created

--- src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
+++ src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 
 import IconBtn from "../../../common/IconBtn"
-
+import { buyCourse } from "../../../../services/operations/studentFeaturesAPI"
 export default function RenderTotalAmount() {
   const { total, cart } = useSelector((state) => state.cart)
   const { token } = useSelector((state) => state.auth)
@@ -12,7 +12,7 @@ export default function RenderTotalAmount() {
 
   const handleBuyCourse = () => {
     const courses = cart.map((course) => course._id)
-    //TODO -> BuyCourse(token, courses, user, navigate, dispatch)
+     buyCourse(token, courses, user, navigate, dispatch)
   }
 
   return (
