7b10bdd3396a0459abb6c0624d489ba0891968ef
shatachhi-10 <137662646+shatachhi-10@users.noreply.github.com>
2024-02-07 15:22:34 +0530

    Instructor Dashboard created

--- src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
+++ src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom"
 
 import IconBtn from "../../../common/IconBtn"
 import { buyCourse } from "../../../../services/operations/studentFeaturesAPI"
+
 export default function RenderTotalAmount() {
   const { total, cart } = useSelector((state) => state.cart)
   const { token } = useSelector((state) => state.auth)
@@ -12,7 +13,7 @@ export default function RenderTotalAmount() {
 
   const handleBuyCourse = () => {
     const courses = cart.map((course) => course._id)
-     buyCourse(token, courses, user, navigate, dispatch)
+    buyCourse(token, courses, user, navigate, dispatch)
   }
 
   return (
